c532123636d7307b5f3fe2d642033eebef0bf49a
osazee25 <osazeemarvins@yahoo.com>
2023-02-18 08:48:30 +0000

    test

---
+++ test5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+   * main - a program that prints the alphabets in lowercase except q and e
+    * Return: 0
+     */
+int main(void)
+{
+		char  ch = 'a';
+
+			while (ch <= 'z')
+					{
+
+							if (ch != 'e' && ch != 'q')
+									{
+											putchar(ch);
+												}
+								ch++;
+
+									}
+
+				putchar('\n');
+					return (0);
+}
---
+++ test6b.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - prints all single digits base 10
+ *Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+	printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
